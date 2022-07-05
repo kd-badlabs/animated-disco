8a00e2ddb3a5dd0d46bbe59f553720c9652dd77c
sourish <badlabs.kd@gmail.com>
2022-07-05 15:57:57 -0400

    update loop

--- datascience.py
+++ datascience.py
@@ -2,6 +2,7 @@ def demo():
     print("hello World")
     for i in range(10):
         print("number {} is printed ".format(i))
+        print("Sourish, number {} is printed ".format(i))
     
     
 if __name__ == "__main__":
