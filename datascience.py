97e8e7c1aa8a060d0ebd6dd90cd6fa788ab4fbf9
sourish <badlabs.kd@gmail.com>
2022-07-05 15:54:16 -0400

    updated for loop

--- datascience.py
+++ datascience.py
@@ -1,6 +1,10 @@
-def  demo():
+def demo():
     print("hello World")
+    for i in range(10):
+        print("number {} is printed ".format(i))
     
     
 if __name__ == "__main__":
     demo()
+    
+    
